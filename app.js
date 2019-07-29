d4747802a12c9ed91e4aa0b8c2de7961dd262573
Gawain Hammond <gawainhammond@ghair.local>
2019-07-29 19:26:54 +0100

    update app.js for heroku naming

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var PORT = process.env['PORT'] || 8089;
+
+console.log('hellonode server running http://host:' +PORT + '/');
+
+
+var server = http.createServer(function(req, res) {
+  console.log('hellonode: GET /');
+  res.writeHead(200);
+  res.end('Hello World!: PORT=' + PORT);
+});
+server.listen(PORT);
