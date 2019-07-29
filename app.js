e0c7b6d3764346a341d1bda2249b509ea10815e3
Gawain Hammond <gawainhammond@ghair.local>
2019-07-29 19:59:38 +0100

    update server output 5th time

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ console.log('hellonode server running http://host:' +PORT + '/');
 var server = http.createServer(function(req, res) {
   console.log('hellonode: GET /');
   res.writeHead(200);
-  res.end('Hello World Four!: PORT=' + PORT);
+  res.end('Hello World Fifth!: PORT=' + PORT);
 });
 server.listen(PORT);
