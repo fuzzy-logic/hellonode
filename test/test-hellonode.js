755864012b92c4ba21875c943d37bc3c2f2016ac
Gawain Hammond <gawainhammond@ghair.local>
2019-07-29 19:21:14 +0100

    added mocha test

---
+++ test/test-hellonode.js
@@ -0,0 +1,47 @@
+const expect = require('expect.js');
+const assert = require('assert');
+const request = require("superagent");
+
+
+
+
+describe('test hello node web app', function() {
+
+
+   before(function(){
+	console.log("before test");
+    })
+
+    afterEach(function(){
+	console.log("after each");
+    })
+
+    after(function(){
+	console.log("after test");
+    })
+
+
+    it('web app runs', function(done) {
+        const server = require('../hello-world-app.js');
+        const url  = 'http://localhost:8089/';
+   	request.get(url)
+   	.then(res => {
+      		// res.text, res.body, res.headers, res.status
+        	console.log(JSON.stringify(res));
+        	expect(res.text).to.be("Hello World!: PORT=8089");
+        	expect(res.status).to.be(200);
+		process.exit(0);
+        	done();
+   	})
+   	.catch(err => {
+      		// err.message, err.response
+		console.log(err.message);
+		console.log(err.response);
+		done(err);
+
+   	});
+    });
+
+});
+
+
